f832dbcfee7682c042456325d3ff4bc66b564fc7
Alex Marz <alexhmarz@gmail.com>
2021-02-02 21:00:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input = document.querySelector('input');
+function addingEventListener () {
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
 }
